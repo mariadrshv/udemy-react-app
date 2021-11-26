c8025e61d22179bc41a7dd16b1b703d7e5164553
Maria Dzeryusheva <mariaxxderyusheva@gmail.com>
2021-11-26 18:09:44 +0300

    Added 'NewExpenses' components (in progress)

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,22 @@
+import React, {useState} from 'react';
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
-import Card from './Card';
+import Card from '../UI/Card';
+
 
 function ExpenseItem ({date, title, amount}) {
+    const [newTitle, setNewTitle] = useState(title);
+    const clickHandler = () => {
+        setNewTitle('Updated!');
+    }
     return (
         <Card className="expense-item">
             <ExpenseDate date={date}/>
             <div className="expense-item__description">
-                <h2>{title}</h2>
+                <h2>{newTitle}</h2>
                 <div className="expense-item__price">${amount}</div>
             </div>
+            <button onClick={clickHandler}>Change title</button>
         </Card>
     )
 }
