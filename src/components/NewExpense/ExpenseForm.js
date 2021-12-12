fba6e58adcfa263a3c1a1a369f0d2a41f082ad87
Maria Dzeryusheva <mariaxxderyusheva@gmail.com>
2021-12-13 00:06:51 +0300

    Added "Add New expense" part

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -55,6 +55,8 @@ const ExpenseForm = (props) => {
         setEnteredTitle('')
     };
 
+
+
     return (
     <form onSubmit={submitHandler}>
         <div className="new-expense__controls">
@@ -74,6 +76,7 @@ const ExpenseForm = (props) => {
             </div>
         </div>
         <div className="new-expense__actions">
+            <button type="button" onClick={props.onCancel}>Cancel</button>
             <button type="submit">Add Expense</button>
         </div>
     </form>
